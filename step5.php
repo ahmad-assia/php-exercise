0ef65aad7d7f9d29c900a7f15b9a1744ee11ac4e
assia ahmad <ahmadassia676@gmail.com>
2022-02-24 16:22:18 +0200

    step5

---
+++ step5.php
@@ -0,0 +1,17 @@
+<?php
+$course_name= 'php';
+$enrolled_students= 20;
+$price= 30.5;
+$on_discount=false;
+ if($on_discount==true){
+ echo "coursetitle: " .$course_name ."\n enrolled students: " .$enrolled_students ." \n  Course price: ".$price . "\n  Course on discount: yes \n";
+ }else {
+ echo "coursetitle: " .$course_name ."\n enrolled students: " .$enrolled_students ." \n  Course price: ".$price . "\n  Course on discount: no  \n";
+}
+
+
+
+
+
+
+?>
