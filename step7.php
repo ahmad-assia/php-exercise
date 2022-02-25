7121a8f82c366f6d355c5c34bc1c4c4521be79cc
assia ahmad <ahmadassia676@gmail.com>
2022-02-25 13:40:18 +0200

    step7

---
+++ step7.php
@@ -0,0 +1,14 @@
+<?php
+$birth_year=1994;
+$current_year= 2022 ;
+
+if (($current_year -$birth_year) >=18 ){
+
+    echo  "You can drive";
+}
+else {
+
+
+echo "You still a kid, go and play GTA";
+}
+?>
